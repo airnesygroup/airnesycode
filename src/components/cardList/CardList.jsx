8228eb719d7ac72e71f6a1d79bae164e6473ffd2
psnapyeditor <info.psnapy@gmail.com>
2024-07-17 19:50:06 +0300

    first main  commit

--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -29,7 +29,6 @@ const CardList = async ({ page, cat }) => {
 
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>Recent Posts</h1>
       <div className={styles.posts}>
         {posts?.map((item) => (
           <Card item={item} key={item._id} />
