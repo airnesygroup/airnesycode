03a7815a43458b994683709595b3e2a23ad2e9db
psnapyeditor <info.psnapy@gmail.com>
2024-08-01 13:47:48 +0300

    first main  commit

--- src/components/Menuposts2/menuposts2.jsx
+++ src/components/Menuposts2/menuposts2.jsx
@@ -42,11 +42,7 @@ const MenuPosts2 = ({ item, withImage }) => {
   return (
     <div className={styles.items}>
       <Link href={`/posts/${item.slug}`} className={styles.item} key={item.slug}>
-        {withImage && (
-          <div className={styles.imageContainer}>
-            <Image src="/profile.png" alt={item.title} fill className={styles.image} />
-          </div>
-        )}
+    
         <div className={styles.textContainer}>
           <h3 className={styles.postTitle}>{item.title.substring(0, 60)}</h3>
           <div className={styles.detail}>
