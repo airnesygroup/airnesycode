2aa9ca57cb6a936b71f499a7875fe4eb5a73d902
psnapyeditor <info.psnapy@gmail.com>
2024-08-01 13:01:05 +0300

    first main  commit

--- src/components/menuPosts/MenuPosts.jsx
+++ src/components/menuPosts/MenuPosts.jsx
@@ -3,40 +3,7 @@ import Link from "next/link";
 import React from "react";
 import styles from "./menuPosts.module.css";
 
-const slugToClassMap = {
-  "general": "general",
-  "news&politics": "news-and-politics",
-  "business&finance": "business-and-finance",
-  "technology": "technology",
-  "science": "science",
-  "q&a": "q-and-a",
-  "health&fitness": "health-and-fitness",
-  "sports": "sports",
-  "gaming": "gaming",
-  "funny": "funny",
-  "music&entertainment": "music-and-entertainment",
-  "movies&tv": "movies-and-tv",
-  "anime": "anime",
-  "pop&culture": "pop-and-culture",
-  "fashion&beauty": "fashion-and-beauty",
-  "lifestyle": "lifestyle",
-  "food&drinks": "food-and-drinks",
-  "arts&crafts": "arts-and-crafts",
-  "hobbies&collectibles": "hobbies-and-collectibles",
-  "places&travel": "places-and-travel",
-  "home&garden": "home-and-garden",
-  "education&career": "education-and-career",
-  "humanities&law": "humanities-and-law",
-  "books": "books",
-  "nature&outdoors": "nature-and-outdoors",
-  "environment&climate": "environment-and-climate",
-  "vehicles&machinery": "vehicles-and-machinery"
-};
-
 const MenuPosts = ({ item, withImage }) => {
-  // Get the CSS class name from the slug
-  const className = slugToClassMap[item.slug] || "default-class";
-
   return (
     <div className={styles.items}>
       <Link href={`/posts/${item.slug}`} className={styles.item} key={item.slug}>
@@ -48,7 +15,6 @@ const MenuPosts = ({ item, withImage }) => {
         <div className={styles.textContainer}>
           <h3 className={styles.postTitle}>{item.title.substring(0, 60)}</h3>
           <div className={styles.detail}>
-            <span className={styles[className]}>{item.category}</span>
           </div>
         </div>
       </Link>
