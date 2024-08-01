e91b682229bb9dab583fc5d9c533d920cda067fa
psnapyeditor <info.psnapy@gmail.com>
2024-08-01 16:13:44 +0300

    first main  commit

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -12,9 +12,7 @@ const Menu = () => {
       <h1 className={styles.title}>Most Popular</h1>
       <Menulists page={1} cat={""} />
 
-      <h2 className={styles.subtitle2}>Chosen by the editor</h2>
-      <h1 className={styles.title}>Editors Pick</h1>
-
+     
     </div>
   );
 };
