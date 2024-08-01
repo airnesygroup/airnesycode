ac4f7207ba60fbf88da4ae9780fefb9424dc6059
psnapyeditor <info.psnapy@gmail.com>
2024-08-01 15:37:52 +0300

    first main  commit

--- src/components/menuPosts/MenuPosts.jsx
+++ src/components/menuPosts/MenuPosts.jsx
@@ -41,17 +41,20 @@ const MenuPosts = ({ item, withImage }) => {
   return (
     <div className={styles.items}>
       <Link href={`/posts/${item.slug}`} className={styles.item} key={item.slug}>
-        {withImage && (
-          <div className={styles.imageContainer}>
-            <Image src="/profile.png" alt={item.title} fill className={styles.image} />
-          </div>
-        )}
-        <div className={styles.textContainer}>
-          {categoryClass && (
+        <div className={styles.imageAndCategory}>
+          {withImage && (
+            <div className={styles.imageContainer}>
+              <Image src="/profile.png" alt={item.title} fill className={styles.image} />
+              {categoryClass && (
             <span className={`${styles.category} ${styles[categoryClass]}`}>
               {item.catSlug}
             </span>
           )}
+            </div>
+          )}
+       
+        </div>
+        <div className={styles.textContainer}>
           <h3 className={styles.postTitle}>{item.title.substring(0, 60)}</h3>
           <div className={styles.detail}>
             <span>{item.category}</span>
