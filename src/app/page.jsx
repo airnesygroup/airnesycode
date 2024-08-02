c49979b6e0ae1ee68e91f40f0ef0560311e8e487
psnapyeditor <info.psnapy@gmail.com>
2024-08-02 15:28:26 +0300

    first main  commit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,3 @@
-"use client";
-
-import { useEffect, useState } from "react";
 import Link from "next/link";
 import styles from "./homepage.module.css";
 import Featured from "@/components/featured/Featured";
@@ -11,37 +8,31 @@ import SidebarCategoryList from "@/components/SidebarcategoryList/SidebarCategor
 import CarouselList from "@/components/carouselList/CarouselList";
 import Footer from "@/components/footer/Footer";
 import Navbar from "@/components/navbar/Navbar";
-import PopUp from "@/components/PopUp/PopUp";
+
 
 export default function Home({ searchParams }) {
-  console.log("Home component rendered");
-
-  const page = parseInt(searchParams?.page) || 1;
-
-  // State to control the visibility of the pop-up
-  const [showPopUp, setShowPopUp] = useState(true);
-
-  // Function to close the pop-up
-  const closePopUp = () => {
-    console.log("Closing pop-up");
-    setShowPopUp(false);
-  };
+  const page = parseInt(searchParams.page) || 1;
 
   return (
     <div className={styles.container}>
-      {showPopUp && <PopUp closePopUp={closePopUp} />} {/* Render the pop-up conditionally */}
-      <Navbar />
+        <Navbar />
+
       <div className={styles.menu}>
         <SidebarCategoryList />
+        
       </div>
+
       <div className={styles.mainContent}>
-        <CategoryList />
+      <CategoryList />
+
         <CarouselList page={page} />
         <div className={styles.content1}>
           <CardList page={page} />
           <Menu />
+
         </div>
         <Footer />
+
       </div>
     </div>
   );
