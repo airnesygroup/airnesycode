5d92bf30b9c939e6e66fe3d14bd3d5c98f9edbea
psnapyeditor <info.psnapy@gmail.com>
2024-08-02 22:24:02 +0300

    first main  commit

--- src/components/popup.js
+++ src/components/popup.js
@@ -1,32 +1,32 @@
-// components/Popup.js
 "use client";
 
 import React from 'react';
-import styles from "./PopUp.module.css"; // Import the CSS module for the parent component
-import Router from 'next/router';
-import { useRouter } from 'next/router';
+import styles from "./popup.module.css"; // Ensure the file name matches
 import Link from 'next/link';
 
 const Popup = ({ onClose }) => {
   return (
     <div className={styles.popupOverlay}>
-    <div className={styles.popupContent}>
-    <button className={styles.closeButton} onClick={onClose}>X</button>
+      <div className={styles.popupContent}>
+        <button className={styles.closeButton} onClick={onClose}>X</button>
 
-      <h2 className={styles.title}>Tell your story!</h2>
-      <p className={styles.description}> We are a community based blog-streaming social platform, 
-      where users can create posts, publish communities and interact with others on a live preview.</p>
-      <p className={styles.description3}> Create an account and share your  strories now!</p>
-      <p className={styles.description3}> Join us on our jopurney or reach us out through our email</p>
-      <p className={styles.description2}> airnesy.info@gmail.com</p>
-      <div className={styles.buttonContainer}>
-        <button className={styles.button} ><Link href="/login" >
-          Sighn up
-        </Link></button>
+        <h2 className={styles.title}>Tell your story!</h2>
+        <p className={styles.description}>We are a community based blog-streaming social platform, 
+          where users can create posts, publish communities and interact with others on a live preview.</p>
+        <p className={styles.description3}>Create an account and share your stories now!</p>
+        <p className={styles.description3}>Join us on our journey or reach out through our email</p>
+        <p className={styles.description2}>airnesy.info@gmail.com</p>
+        <div className={styles.buttonContainer}>
+          <button className={styles.button}>
+            <Link href="/login">
+              Sign up
+            </Link>
+          </button>
+        </div>
       </div>
     </div>
-  </div>
   );
 };
 
 export default Popup;
+
