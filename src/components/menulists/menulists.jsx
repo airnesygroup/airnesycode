2753fce01ffe1c214bea8f0bd96d9d3156fa6574
psnapyeditor <info.psnapy@gmail.com>
2024-08-02 20:16:25 +0300

    first main  commit

--- src/components/menulists/menulists.jsx
+++ src/components/menulists/menulists.jsx
@@ -1,39 +1,37 @@
-// components/MenuLists.jsx
-import React, { useEffect, useState } from "react";
+// src/components/menuLists/MenuListsServer.jsx
+"use client"; // Use this if this file is being used in the client-side context
+
+import React, { useState, useEffect } from "react";
 import MenuPosts from "../menuPosts/MenuPosts";
 import styles from "./menulists.module.css";
 
 const MenuListsServer = ({ page, cat }) => {
   const [posts, setPosts] = useState([]);
-  const [count, setCount] = useState(0);
+  const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-
+  
   useEffect(() => {
     const fetchData = async () => {
       try {
         const res = await fetch(
-          `https://www.airnesy.com/api/post2?page=${page}&cat=${cat || ""}`
+          `/api/post2?page=${page}&cat=${cat || ""}`
         );
-        if (!res.ok) {
-          throw new Error("Failed to fetch data");
-        }
+        if (!res.ok) throw new Error("Failed to fetch data");
+        
         const data = await res.json();
         setPosts(data.posts);
-        setCount(data.count);
-      } catch (err) {
-        setError(err.message);
+        setLoading(false);
+      } catch (error) {
+        setError(error.message);
+        setLoading(false);
       }
     };
 
     fetchData();
   }, [page, cat]);
 
-  const POST_PER_PAGE = 4;
-  const hasPrev = POST_PER_PAGE * (page - 1) > 0;
-  const hasNext = POST_PER_PAGE * (page - 1) + POST_PER_PAGE < count;
-
-  if (error) return <div>Error: {error}</div>;
-  if (!posts.length) return <div>Loading...</div>;
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error}</p>;
 
   return (
     <div className={styles.container}>
