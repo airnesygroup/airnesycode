9ca6360c16dd347cf1be4247c4b6b8da37c1ade9
psnapyeditor <info.psnapy@gmail.com>
2024-08-02 19:37:08 +0300

    first main  commit

--- src/components/menulists/menulists.jsx
+++ src/components/menulists/menulists.jsx
@@ -5,31 +5,39 @@ import styles from "./menulists.module.css";
 
 // Define a server component to fetch data and pass it to the client component
 const MenuListsServer = async ({ page, cat }) => {
-  const res = await fetch(
-    `https://www.airnesy.com/api/post2?page=${page}&cat=${cat || ""}`,
-    {
-      cache: "no-store",
+  try {
+    const res = await fetch(
+      `https://www.airnesy.com/api/post2?page=${page}&cat=${cat || ""}`,
+      {
+        cache: "no-store",
+      }
+    );
+
+    if (!res.ok) {
+      const errorDetails = await res.text();
+      throw new Error(`Failed to fetch data: ${errorDetails}`);
     }
-  );
 
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
+    const { posts, count } = await res.json();
+    const POST_PER_PAGE = 4;
+    const hasPrev = POST_PER_PAGE * (page - 1) > 0;
+    const hasNext = POST_PER_PAGE * (page - 1) + POST_PER_PAGE < count;
 
-  const { posts, count } = await res.json();
-  const POST_PER_PAGE = 4;
-  const hasPrev = POST_PER_PAGE * (page - 1) > 0;
-  const hasNext = POST_PER_PAGE * (page - 1) + POST_PER_PAGE < count;
-
-  return (
-    <div className={styles.container}>
-      <div className={styles.posts}>
-        {posts.map((item) => (
-          <MenuPosts item={item} withImage={true} key={item._id} />
-        ))}
+    return (
+      <div className={styles.container}>
+        <div className={styles.posts}>
+          {posts.map((item) => (
+            <MenuPosts item={item} withImage={true} key={item._id} />
+          ))}
+        </div>
       </div>
-    </div>
-  );
+    );
+  } catch (error) {
+    console.error("Error fetching data:", error.message);
+    return <div>Error loading posts.</div>;
+  }
 };
+
+
 
 export default MenuListsServer;
