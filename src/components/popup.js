3e9080084cb000fe7dfda9e5eaaacea61437382d
psnapyeditor <info.psnapy@gmail.com>
2024-08-03 17:38:52 +0300

    first main  commit

--- src/components/popup.js
+++ src/components/popup.js
@@ -18,7 +18,7 @@ const Popup = ({ onClose }) => {
         <p className={styles.description}>We are a community based blog-streaming social platform, 
           where users can create posts, publish communities and interact with others on a live preview.</p>
         <p className={styles.description3}>Create an account and share your stories now!</p>
-        <p className={styles.description3}>Join us on our journey or reach out through our email</p>
+        <p className={styles.description3}>Join us on our journey or GET IN TOUCH with us through our email</p>
         <p className={styles.description2}>airnesy.info@gmail.com</p>
         <div className={styles.buttonContainer}>
           <button className={styles.button}>
