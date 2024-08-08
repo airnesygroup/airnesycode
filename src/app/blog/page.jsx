315f41d6eb578fca6f6dd77fb544a00afa21ca8f
psnapyeditor <info.psnapy@gmail.com>
2024-08-08 11:36:48 +0300

    first main  commit

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -2,6 +2,7 @@ import CardList from "@/components/cardList/CardList";
 import styles from "./blogPage.module.css";
 import Menu from "@/components/Menu/Menu";
 import Navbar from "@/components/navbar/Navbar";
+import SidebarCategoryList from "@/components/SidebarcategoryList/SidebarCategoryList";
 
 const BlogPage = ({ searchParams }) => {
   const page = parseInt(searchParams.page) || 1;
@@ -10,9 +11,13 @@ const BlogPage = ({ searchParams }) => {
   return (
     <div className={styles.container}>
               <Navbar />
+      <div className={styles.menu}>
+        <SidebarCategoryList />
+      </div>
 
-      <h1 className={styles.title}>{cat} </h1>
       <div className={styles.content}>
+      <h1 className={styles.title}>{cat} </h1>
+
         <CardList page={page} cat={cat}/>
         <Menu />
       </div>
