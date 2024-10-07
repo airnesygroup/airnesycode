59131b7ce34f53251eba1260653d464ca1217277
psnapyeditor <info.psnapy@gmail.com>
2024-10-07 22:57:28 +0300

    second

--- src/app/api/postsimg/route.js
+++ src/app/api/postsimg/route.js
@@ -15,20 +15,28 @@ export const GET = async (req) => {
         user: true, // Include user details
       },
       orderBy: {
-        createdAt: 'desc', // Order by newest posts first
+        createdAt: "desc", // Order by newest posts first
       },
     });
+
+    // Return if no posts found
+    if (allPosts.length === 0) {
+      return new NextResponse(
+        JSON.stringify({ posts: [], count: 0 }),
+        { status: 200 }
+      );
+    }
 
     // Group posts into 24-hour batches
     let currentBatch = [];
     let batchedPosts = [];
-    let currentTime = new Date(allPosts[0]?.createdAt || new Date());
-    let previousTime = new Date(currentTime.getTime() - 24 * 60 * 60 * 1000); // Set the previous time to 24 hours earlier
+    let currentTime = new Date(allPosts[0].createdAt);
+    let previousTime = new Date(currentTime.getTime() - 24 * 60 * 60 * 1000); // 24 hours earlier
 
     for (const post of allPosts) {
       const postDate = new Date(post.createdAt);
 
-      // If the post is within the current 24-hour batch, add it to the current batch
+      // Check if the post is within the current 24-hour batch
       if (postDate >= previousTime && postDate < currentTime) {
         currentBatch.push(post);
       } else {
@@ -38,14 +46,14 @@ export const GET = async (req) => {
           batchedPosts = [...batchedPosts, ...randomizedBatch];
         }
 
-        // Reset the batch and set the new time frame
+        // Reset the batch and update time window
         currentBatch = [post];
         currentTime = previousTime;
         previousTime = new Date(currentTime.getTime() - 24 * 60 * 60 * 1000);
       }
     }
 
-    // Don't forget to add the last batch after the loop
+    // Add the last batch after the loop
     if (currentBatch.length > 0) {
       const randomizedBatch = currentBatch.sort(() => Math.random() - 0.5);
       batchedPosts = [...batchedPosts, ...randomizedBatch];
@@ -53,8 +61,10 @@ export const GET = async (req) => {
 
     // Return the final list of posts
     const totalCount = allPosts.length;
-
-    return new NextResponse(JSON.stringify({ posts: batchedPosts, count: totalCount }), { status: 200 });
+    return new NextResponse(
+      JSON.stringify({ posts: batchedPosts, count: totalCount }),
+      { status: 200 }
+    );
   } catch (err) {
     console.error("Error fetching posts:", err);
     return new NextResponse(
