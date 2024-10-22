c884f066f45946da101333126c14ba3765b56296
airnesygroup <airnesy.info@gmail.com>
2024-10-22 20:35:26 +0300

    main commit

--- src/providers/ThemeProvider.jsx
+++ src/providers/ThemeProvider.jsx
@@ -9,22 +9,11 @@ const ThemeProvider = ({ children }) => {
 
   useEffect(() => {
     setMounted(true);
+  }, []);
 
-    // Dynamically change the system navigation bar color
-    const metaThemeColor = document.querySelector('meta[name="theme-color"]');
-    
-    if (theme === "dark") {
-      metaThemeColor.setAttribute("content", "#000000"); // Set dark color for the navigation bar
-    } else {
-      metaThemeColor.setAttribute("content", "#ffffff"); // Set light color for the navigation bar
-    }
-  }, [theme]);
-
-  if (!mounted) {
-    return null;
+  if (mounted) {
+    return <div className={theme}>{children}</div>;
   }
-
-  return <div className={theme}>{children}</div>;
 };
 
 export default ThemeProvider;
