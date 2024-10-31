26cd160470b7d248d9d1d13dcdca5142c3985219
airnesygroup <airnesy.info@gmail.com>
2024-11-01 00:49:47 +0300

    Initial commit

--- src/app/posts/[slug]/page.jsx
+++ src/app/posts/[slug]/page.jsx
@@ -34,24 +34,27 @@ const SinglePage = async ({ params }) => {
       <div className={styles.mainContent}>
         
         <div className={styles.content}>
+          <div className={styles.post}>
+            
         {data?.img && (
             <div className={styles.imageContainer}>
               <Image src={data.img} alt="" fill className={styles.image} />
             </div>
           )}
-          <div className={styles.post}>
-          <h1 className={styles.title}>{data?.title}</h1>
           <div className={styles.user}>
             {data?.user?.image && (
               <div className={styles.userImageContainer}>
                 <Image src={data.user.image} alt="" fill className={styles.avatar} />
               </div>
             )}
+            
+
             <div className={styles.userTextContainer}>
               <span className={styles.username}>{data?.user.name}</span>
               <span className={styles.date}>{data?.date || '01.01.2024'}</span>
             </div>
           </div>
+          <h1 className={styles.title}>{data?.title}</h1>
 
             <div 
              className={styles.description}
