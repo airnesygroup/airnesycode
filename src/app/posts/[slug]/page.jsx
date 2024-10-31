39ace1ade33ccde8d2286cda75fd94c26b501eb6
airnesygroup <airnesy.info@gmail.com>
2024-11-01 01:18:39 +0300

    Initial commit

--- src/app/posts/[slug]/page.jsx
+++ src/app/posts/[slug]/page.jsx
@@ -5,9 +5,6 @@ import Comments from "@/components/comments/Comments";
 import SidebarCategoryList from "@/components/SidebarcategoryList/SidebarCategoryList";
 import Navbar from "@/components/navbar/Navbar";
 
-
-
-
 const getData = async (slug) => {
   const res = await fetch(`https://www.airnesy.com/api/posts/${slug}`, {
     cache: "no-store",
@@ -26,65 +23,47 @@ const SinglePage = async ({ params }) => {
   const data = await getData(slug);
 
   return (
-
-
-
-
-
-
-
-<div className={styles.container}>
-
-
-<Navbar />
-
-<div className={styles.menu}>
-  <SidebarCategoryList />
-</div>
-<div className={styles.mainContent}>
-
-
-  <div className={styles.content1}>
-  <div className={styles.content}>
-          <div className={styles.post}>
-            
-        {data?.img && (
-            <div className={styles.imageContainer}>
-              <Image src={data.img} alt="" fill className={styles.image} />
-            </div>
-          )}
-          <div className={styles.user}>
-            {data?.user?.image && (
-              <div className={styles.userImageContainer}>
-                <Image src={data.user.image} alt="" fill className={styles.avatar} />
+    <div className={styles.container}>
+      <Navbar />
+      <div className={styles.contentWrapper}>
+        <div className={styles.menu}>
+          <SidebarCategoryList />
+        </div>
+        <div className={styles.mainContent}>
+          <div className={styles.content1}>
+            <div className={styles.content}>
+              <div className={styles.post}>
+                {data?.img && (
+                  <div className={styles.imageContainer}>
+                    <Image src={data.img} alt="" fill className={styles.image} />
+                  </div>
+                )}
+                <div className={styles.user}>
+                  {data?.user?.image && (
+                    <div className={styles.userImageContainer}>
+                      <Image src={data.user.image} alt="" fill className={styles.avatar} />
+                    </div>
+                  )}
+                  <div className={styles.userTextContainer}>
+                    <span className={styles.username}>{data?.user.name}</span>
+                    <span className={styles.date}>{data?.date || '01.01.2024'}</span>
+                  </div>
+                </div>
+                <h1 className={styles.title}>{data?.title}</h1>
+                <div
+                  className={styles.description}
+                  dangerouslySetInnerHTML={{ __html: data?.desc }}
+                />
+                <div className={styles.comment}>
+                  <Comments postSlug={slug} />
+                </div>
               </div>
-            )}
-            
-
-            <div className={styles.userTextContainer}>
-              <span className={styles.username}>{data?.user.name}</span>
-              <span className={styles.date}>{data?.date || '01.01.2024'}</span>
+              <Menu />
             </div>
           </div>
-          <h1 className={styles.title}>{data?.title}</h1>
-
-            <div 
-             className={styles.description}
-              dangerouslySetInnerHTML={{ __html: data?.desc }}
-            />
-            <div className={styles.comment}>
-              <Comments postSlug={slug} />
-            </div>
-          </div>
-    <Menu />
-  </div>
-</div>
-</div>
-
-
-</div>
-
-
+        </div>
+      </div>
+    </div>
   );
 };
 
