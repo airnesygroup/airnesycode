9c809f1650b28ef8cf2842846a7c03bf042c1adc
airnesygroup <airnesy.info@gmail.com>
2024-11-01 01:40:06 +0300

    Initial commit

--- src/app/posts/[slug]/page.jsx
+++ src/app/posts/[slug]/page.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import Comments from "@/components/comments/Comments";
 import SidebarCategoryList from "@/components/SidebarcategoryList/SidebarCategoryList";
 import Navbar from "@/components/navbar/Navbar";
-
+import Footer from "@/components/Footer"; // Ensure to import Footer
 
 const getData = async (slug) => {
   const res = await fetch(`https://www.airnesy.com/api/posts/${slug}`, {
@@ -12,7 +12,7 @@ const getData = async (slug) => {
   });
 
   if (!res.ok) {
-    throw new Error("Failed");
+    throw new Error("Failed to fetch data");
   }
 
   return res.json();
@@ -20,75 +20,52 @@ const getData = async (slug) => {
 
 const SinglePage = async ({ params }) => {
   const { slug } = params;
-
   const data = await getData(slug);
 
   return (
-  
+    <div className={styles.container}>
+      <Navbar />
 
-
-
-
-
-
-
-
-
-
-
-<div className={styles.container}>
-<Navbar />
-
-<div className={styles.menu}>
-  <SidebarCategoryList />
-</div>
-<div className={styles.mainContent}>
-
-
-
-
-
-
-  <div className={styles.content1}>
-
-        <div className={styles.contentia}>
-          <div className={styles.post}>
-            
-          {data?.img && (
-            <div className={styles.imageContainer}>
-              <Image src={data.img} alt="" fill className={styles.image} />
-            </div>
-          )}
-          <h1 className={styles.title}>{data?.title}</h1>
-          <div className={styles.user}>
-            {data?.user?.image && (
-              <div className={styles.userImageContainer}>
-                <Image src={data.user.image} alt="" fill className={styles.avatar} />
+      <div className={styles.menu}>
+        <SidebarCategoryList />
+      </div>
+      
+      <div className={styles.mainContent}>
+        <div className={styles.content1}>
+          <div className={styles.contentia}>
+            <div className={styles.post}>
+              {data?.img && (
+                <div className={styles.imageContainer}>
+                  <Image src={data.img} alt="" fill className={styles.image} />
+                </div>
+              )}
+              <h1 className={styles.title}>{data?.title}</h1>
+              <div className={styles.user}>
+                {data?.user?.image && (
+                  <div className={styles.userImageContainer}>
+                    <Image src={data.user.image} alt="" fill className={styles.avatar} />
+                  </div>
+                )}
+                <div className={styles.userTextContainer}>
+                  <span className={styles.username}>{data?.user.name}</span>
+                  <span className={styles.date}>{data?.date || '01.01.2024'}</span>
+                </div>
               </div>
-            )}
-            <div className={styles.userTextContainer}>
-              <span className={styles.username}>{data?.user.name}</span>
-              <span className={styles.date}>{data?.date || '01.01.2024'}</span>
-            </div>
-          </div>
 
-            <div 
-             className={styles.description}
-              dangerouslySetInnerHTML={{ __html: data?.desc }}
-            />
-            <div className={styles.comment}>
-              <Comments postSlug={slug} />
+              <div 
+                className={styles.description}
+                dangerouslySetInnerHTML={{ __html: data?.desc }}
+              />
+              <div className={styles.comment}>
+                <Comments postSlug={slug} />
+              </div>
             </div>
+            <Menu />
           </div>
-          <Menu />
         </div>
-  </div>
-  <Footer />
-</div>
-</div>
-
-
-        
+        <Footer />
+      </div>
+    </div>
   );
 };
 
