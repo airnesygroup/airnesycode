fcf1c5e15b7f88122eb819a71e5411db889553e9
airnesygroup <airnesy.info@gmail.com>
2024-11-05 10:33:23 +0300

    Initial commit

--- src/app/posts/[slug]/page.jsx
+++ src/app/posts/[slug]/page.jsx
@@ -11,7 +11,7 @@ const getData = async (slug) => {
   });
 
   if (!res.ok) {
-    throw new Error("Failed to fetch data");
+    throw new Error("Failed");
   }
 
   return res.json();
@@ -22,45 +22,39 @@ const SinglePage = async ({ params }) => {
   const data = await getData(slug);
 
   return (
-    <div className={styles.container}>
+    <div className={styles.containeria}>
       <Navbar />
-
-      <div className={styles.menu}>
+      <div className={styles.menulia}>
         <SidebarCategoryList />
       </div>
-      
-      <div className={styles.mainContent}>
-        <div className={styles.content1}>
-          <div className={styles.contentia}>
-            <div className={styles.post}>
-              {data?.img && (
-                <div className={styles.imageContainer}>
-                  <Image src={data.img} alt="" fill className={styles.image} />
+      <div className={styles.mainContentia}>
+        <div className={styles.contentia}>
+          <div className={styles.postia}>
+          <div className={styles.imageContainer}>
+    <Image src={data.img} alt="" fill className={styles.image} />
+  </div>
+)}
+            <h1 className={styles.titleia}>{data?.title}</h1>
+            <div className={styles.useria}>
+              {data?.user?.image && (
+                <div className={styles.userImageContaineria}>
+                  <Image src={data.user.image} alt="" fill className={styles.avataria} />
                 </div>
               )}
-              <h1 className={styles.title}>{data?.title}</h1>
-              <div className={styles.user}>
-                {data?.user?.image && (
-                  <div className={styles.userImageContainer}>
-                    <Image src={data.user.image} alt="" fill className={styles.avatar} />
-                  </div>
-                )}
-                <div className={styles.userTextContainer}>
-                  <span className={styles.username}>{data?.user.name}</span>
-                  <span className={styles.date}>{data?.date || '01.01.2024'}</span>
-                </div>
-              </div>
-
-              <div 
-                className={styles.description}
-                dangerouslySetInnerHTML={{ __html: data?.desc }}
-              />
-              <div className={styles.comment}>
-                <Comments postSlug={slug} />
+              <div className={styles.userTextContaineria}>
+                <span className={styles.usernameia}>{data?.user.name}</span>
+                <span className={styles.dateia}>{data?.date || '01.01.2024'}</span>
               </div>
             </div>
-            <Menu />
+            <div
+              className={styles.descriptionia}
+              dangerouslySetInnerHTML={{ __html: data?.desc }}
+            />
+            <div className={styles.commentia}>
+              <Comments postSlug={slug} />
+            </div>
           </div>
+          <Menu />
         </div>
       </div>
     </div>
@@ -68,3 +62,4 @@ const SinglePage = async ({ params }) => {
 };
 
 export default SinglePage;
+
