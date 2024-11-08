3dfa6780f49527c8181cc5be7e5fdc6b4b242d05
airnesygroup <airnesy.info@gmail.com>
2024-11-08 14:16:59 +0300

    Initial commit

--- src/components/ParentComponent.js
+++ src/components/ParentComponent.js
@@ -1,8 +1,23 @@
-import { useState } from "react";
+import { useEffect } from "react";
 import WritePage from "./write/page";
 import styles from "./ParentComponent.module.css"; // Import the CSS module for the parent component
 
 const Modal = ({ open, onClose, children }) => {
+  useEffect(() => {
+    if (open) {
+      // Disable scrolling when the modal is open
+      document.body.style.overflow = "hidden";
+    } else {
+      // Re-enable scrolling when the modal is closed
+      document.body.style.overflow = "auto";
+    }
+
+    // Clean up the effect when the component is unmounted or modal is closed
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [open]);
+
   if (!open) return null;
 
   return (
