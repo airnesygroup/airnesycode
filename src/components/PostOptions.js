c572b51df4f4d29c4c55d873bcb17b8c08c65a91
airnesygroup <airnesy.info@gmail.com>
2024-11-13 10:45:38 +0300

    Initial commit

--- src/components/PostOptions.js
+++ src/components/PostOptions.js
@@ -1,3 +1,6 @@
+
+'use client';
+
 import React, { useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShareAlt, faTrash } from '@fortawesome/free-solid-svg-icons';
