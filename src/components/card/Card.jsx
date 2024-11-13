bd8c2c59f78c290387d63c93a3529174a246d3c0
airnesygroup <airnesy.info@gmail.com>
2024-11-13 12:39:27 +0300

    Initial commit

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,28 +1,22 @@
-import React from "react";
+import Image from "next/image";
 import styles from "./card.module.css";
 import Link from "next/link";
-import { formatDistanceToNow } from "date-fns";
-import Image from "next/image";
-import PostOptions from "../PostOptions";
-const Card = ({ item, userEmail, onPostDeleted }) => {
+import { formatDistanceToNow } from 'date-fns';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons'; // Import the verified icon
+
+
+
+
+const Card = ({ key, item }) => {
   const truncatedDesc = item?.desc.substring(0, 500);
   const truncatedDesc2 = item?.desc.substring(0, 140);
-  
-  const showMore = item?.desc.length > 300;
 
-  // Handler for the delete options click
-  const handleOptionsClick = (e) => {
-    // Prevent the Link navigation
-    e.stopPropagation();
-    e.preventDefault();
-    // Optionally trigger delete or other actions
-    console.log('Delete clicked');
-    onPostDeleted(item.id); // Call the delete function with the post ID
-  };
+  const showMore = item?.desc.length > 300;
 
   return (
     <Link href={`/posts/${item.slug}`} passHref>
-      <div className={styles.container}>
+      <div className={styles.container} key={key}>
         <div className={styles.profileContainer}>
           <Image
             src={item.user?.image}
@@ -49,23 +43,26 @@ const Card = ({ item, userEmail, onPostDeleted }) => {
                     <p className={styles.username}>{item.user?.name.substring(0, 10)}</p>
                     <p className={styles.userRole}>{item.user?.role}</p>
                   </div>
-                  <img
-                    src="/verified.png"
-                    alt="Verified"
-                    className={styles.verifiedIcon}
-                  />
+                  <img 
+src="/verified.png"     alt="Verified" 
+className={styles.verifiedIcon} 
+    />
                   <span className={styles.date}>
-                    {formatDistanceToNow(new Date(item.createdAt), { addSuffix: true }).substring(0, 13)}
+                  {formatDistanceToNow(new Date(item.createdAt), { addSuffix: true }).substring(0, 13)}
                   </span>
+
                 </div>
               </div>
             </div>
 
+
             <span className={styles.category}>{item.catSlug}</span>
-            {/* Add the delete button here */}
-            <button onClick={handleOptionsClick} className={styles.optionsButton}>...</button>
+
+
+            <span className={styles.span}>...</span>
           </div>
           <h1 className={styles.title}>{item.title.substring(0, 150)}</h1>
+          <h1 className={styles.title2}>{item.title.substring(0,150)}</h1>
 
           <div className={styles.descContainer}>
             <div
@@ -76,6 +73,7 @@ const Card = ({ item, userEmail, onPostDeleted }) => {
               className={styles.desc2}
               dangerouslySetInnerHTML={{ __html: truncatedDesc2 }}
             />
+        
           </div>
 
           {item.img && (
@@ -86,19 +84,21 @@ const Card = ({ item, userEmail, onPostDeleted }) => {
                   backgroundImage: `url(${item.img})`,
                 }}
               />
-              <Image
+               <Image
                 src={item.img}
                 alt={item.title}
                 layout="intrinsic"
                 className={styles.image}
               />
+              {/* Move the horizontal line here */}
             </div>
           )}
-          
-          {/* Post Options */}
-          <PostOptions post={item} onDelete={onPostDeleted} />
+
         </div>
+
       </div>
+      <div className={styles.horizontalLine}></div>
+
     </Link>
   );
 };
